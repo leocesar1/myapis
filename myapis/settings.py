7ea9061892b92b7033ebd23473d74194579705c7
Leonardo Marques <leocesar.rj@gmail.com>
2021-12-16 12:35:37 -0300

    Debug false

--- myapis/settings.py
+++ myapis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m0h)puvb_sc+8uvgbv)3q+by3rw(n$b#xto)g-ytj3w0^lv0x@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
